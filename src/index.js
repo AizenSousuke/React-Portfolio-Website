42284ca4979a14d9a01d07e677bbf210d832ca2c
AizenSousuke <puteranikdimas@yahoo.com.sg>
2020-05-21 16:01:50 +0800

    Bug fixes

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 // import "./index.css";
-import Home from "./pages/home/Home";
+import Home from "./pages/home/Homepage";
 import Projects from "./pages/projects/Projects";
 import Social from "./pages/social/Social";
 import Navbar from "./components/navbar/navbar";
