a860cdd49e51b12c8980c16bb79084ec665e3660
AizenSousuke <puteranikdimas@yahoo.com.sg>
2020-05-25 12:29:08 +0800

    Added shimmer in project's page

--- src/components/project-tile/project-tile.js
+++ src/components/project-tile/project-tile.js
@@ -1,18 +1,34 @@
 import React from "react";
 import Button from "../buttons/button.js";
+import Image, { Shimmer } from "react-shimmer";
+import ContainerDimensions from 'react-container-dimensions';
 
 const ProjectTile = (props) => {
 	return (
 		<article className={`tile is-parent ${props.tileSize ?? ""}`}>
 			<article className="tile is-child box">
 				<figure className={`image ${props.imageStyle ?? "is-16by9"}`}>
-					<img
+					{/* <img
 						src={`${
 							props.imageLink ??
 							"http://loremflickr.com/480/272/default"
 						}`}
 						alt="button"
-					/>
+					/> */}
+							<Image
+								src={`${
+									props.imageLink ??
+									"http://loremflickr.com/480/272/default"
+								}`}
+								alt="button"
+								fallback={
+									<ContainerDimensions>
+										{({width, height}) => 
+											//<Shimmer width={width} height={height} />
+											<Shimmer width={width > 480 ? 480 : width} height={height > 272 ? 272 : height} />
+										}
+									</ContainerDimensions>}
+							/>
 				</figure>
 				<h1 className="title">{props.title ?? "Title"}</h1>
 				<h2 className="subtitle">{props.subtitle ?? "Subtitle"}</h2>
