3fc9f86bd792fec514812f6a76c18f8a0080c543
AizenSousuke <puteranikdimas@yahoo.com.sg>
2021-02-13 10:42:47 +0800

    Added some updates

--- src/components/timeline/timeline-card.js
+++ src/components/timeline/timeline-card.js
@@ -6,15 +6,15 @@ function TimelineCard(props) {
 			<div className="timeline-marker is-icon is-danger">
 				<i className="fa fa-flag"></i>
 			</div>
-			<div className="timeline-content fullwidth">
+			<div className="timeline-content fullwidth" style={{minWidth: 500}}>
 				<p className="heading">{props.data.month ?? "May 2012"}</p>
 				<div className="message is-info">
 					<div className="message-header">
-						<div className="column">{props.data.header ?? "header"}</div>
+						<div className="column title is-5">{props.data.header ?? "header"}</div>
 						<div className="column tag is-link has-text-weight-bold">{props.data.subtitle ?? "subtitle"}</div>
 					</div>
 					<div className="message-body">
-						<p>{props.data.jobscope ?? ""}</p>
+						<div classname="subtitle">{props.data.jobscope ?? "jobscope"}</div>
 						<div className="is-size-7">
 							{props.data.content ??
 								"Ex anim do id aute aute. Quis anim aliqua eu eiusmod occaecat. Consectetur reprehenderit do commodo do. Occaecat eu magna in deserunt. Qui in aliquip ipsum consequat ipsum ea reprehenderit."}
