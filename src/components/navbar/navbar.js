29a924e4a8e1e5a8832f99adc3ba03f27c43ec2d
AizenSousuke <puteranikdimas@yahoo.com.sg>
2021-02-28 15:11:20 +0800

    Added new animations

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,3 +1,4 @@
+import { motion } from "framer-motion";
 import React from "react";
 // import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import { Link } from "react-router-dom";
@@ -11,28 +12,36 @@ export default function Navbar() {
 		>
 			<div className="navbar-brand">
 				<span className="navbar-item">
-					<i className="fas fa-home"></i>
-					<Link to="/" className="">
-						Home
-					</Link>
+					<motion.div whileHover={{ scale: 1.2 }}>
+						<i className="fas fa-home"></i>
+						<Link to="/" className="">
+							Home
+						</Link>
+					</motion.div>
 				</span>
 				<span className="navbar-item">
-					<i className="fas fa-tasks"></i>
-					<Link to="/projects" className="">
-						Projects
-					</Link>
+					<motion.div whileHover={{ scale: 1.2 }}>
+						<i className="fas fa-tasks"></i>
+						<Link to="/projects" className="">
+							Projects
+						</Link>
+					</motion.div>
 				</span>
 				<span className="navbar-item">
-					<i className="fas fa-history"></i>
-					<Link to="/experiences" className="">
-						Experiences
-					</Link>
+					<motion.div whileHover={{ scale: 1.2 }}>
+						<i className="fas fa-history"></i>
+						<Link to="/experiences" className="">
+							Experiences
+						</Link>
+					</motion.div>
 				</span>
 				<span className="navbar-item">
-					<i className="fas fa-user-circle"></i>
-					<Link to="/about" className="">
-						About
-					</Link>
+					<motion.div whileHover={{ scale: 1.2 }}>
+						<i className="fas fa-user-circle"></i>
+						<Link to="/about" className="">
+							About
+						</Link>
+					</motion.div>
 				</span>
 			</div>
 		</nav>
