aa5e50f1cd6c68f6f6e9dae68a9fd9176da9521e
AizenSousuke <puteranikdimas@yahoo.com.sg>
2021-07-10 22:17:22 +0800

    Added lightbox

--- src/pages/home/Homepage.js
+++ src/pages/home/Homepage.js
@@ -33,82 +33,96 @@ class Home extends Component {
 		}
 
 		return (
-			<div className="container is-fluid">
-				<div className="hero">
-					<div className="hero is-large">
-						<div className="hero-body">
-							<div className="container">
-								<motion.div
-									initial={{
-										rotate: -40,
-										opacity: 0,
-										x: 200,
-									}}
-									animate={{ rotate: -3, opacity: 1, x: 0 }}
-									transition={{
-										type: "spring",
-										damping: 7,
-										velocity: 2,
-										duration: 0.5,
-									}}
-								>
-									<div className="title is-size-1 has-text-weight-bold has-text-right">
-										{this.state.data.title}
-									</div>
-								</motion.div>
-								<br />
-								<br />
-								<motion.div
-									initial={{
-										rotate: 40,
-										opacity: 0,
-										x: -200,
-									}}
-									animate={{ rotate: 0, opacity: 1, x: 0 }}
-									transition={{
-										type: "spring",
-										damping: 7,
-										velocity: 1,
-										duration: 0.5,
-									}}
-								>
-									<div className="subtitle is-size-4 has-text-justified">
-										{this.state.data.subtitle}
-									</div>
-								</motion.div>
-								<br></br>
-								<motion.div
-									initial={{
-										rotateZ: -360,
-										opacity: 0,
-										y: 200,
-									}}
-									animate={{ rotateZ: 0, opacity: 1, y: 0 }}
-									transition={{
-										type: "spring",
-										damping: 7,
-										velocity: 2,
-										duration: 0.3,
-									}}
-									whileHover={{
-										scale: 1.1
-									}}
-								>
-									<NavButton
-										buttonName={
-											this.state.data.subtitle_button
-										}
-										buttonStyle="is-dark is-large"
-										buttonAlign="is-centered"
-										buttonLink="/about"
-										animate=""
-									/>
-								</motion.div>
+			<>
+				<div className="container is-fluid">
+					<div className="hero">
+						<div className="hero is-large">
+							<div className="hero-body">
+								<div className="container">
+									<motion.div
+										initial={{
+											rotate: -40,
+											opacity: 0,
+											x: 200,
+										}}
+										animate={{
+											rotate: -3,
+											opacity: 1,
+											x: 0,
+										}}
+										transition={{
+											type: "spring",
+											damping: 7,
+											velocity: 2,
+											duration: 0.5,
+										}}
+									>
+										<div className="title is-size-1 has-text-weight-bold has-text-right">
+											{this.state.data.title}
+										</div>
+									</motion.div>
+									<br />
+									<br />
+									<motion.div
+										initial={{
+											rotate: 40,
+											opacity: 0,
+											x: -200,
+										}}
+										animate={{
+											rotate: 0,
+											opacity: 1,
+											x: 0,
+										}}
+										transition={{
+											type: "spring",
+											damping: 7,
+											velocity: 1,
+											duration: 0.5,
+										}}
+									>
+										<div className="subtitle is-size-4 has-text-justified">
+											{this.state.data.subtitle}
+										</div>
+									</motion.div>
+									<br></br>
+									<motion.div
+										initial={{
+											rotateZ: -360,
+											opacity: 0,
+											y: 200,
+										}}
+										animate={{
+											rotateZ: 0,
+											opacity: 1,
+											y: 0,
+										}}
+										transition={{
+											type: "spring",
+											damping: 7,
+											velocity: 2,
+											duration: 0.3,
+										}}
+										whileHover={{
+											scale: 1.1,
+										}}
+									>
+										<NavButton
+											buttonName={
+												this.state.data.subtitle_button
+											}
+											buttonStyle="is-dark is-large"
+											buttonAlign="is-centered"
+											buttonLink="/about"
+											animate=""
+										/>
+									</motion.div>
+								</div>
 							</div>
 						</div>
 					</div>
 				</div>
-			</div>
+			</>
 		);
 	}
 }
